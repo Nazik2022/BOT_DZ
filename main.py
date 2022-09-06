330081ce97690ef56783e9a126777349571edd95
Nazik2022 <Nazik_Setembaeva@mail.ru>
2022-09-06 16:25:22 +0600

    added heroku

--- main.py
+++ main.py
@@ -2,26 +2,24 @@
 from aiogram.utils import executor
 from config import dp
 
-from handlers import  extra
-# from databace import bot_dp
-# import asyncio
+from handlers import extra, client, callback, fsmAdminMenu, notification
+from databace import bot_dp
+import asyncio
 
 
-# async def on_startup(_):
-#     asyncio.create_task(notification.scheduler())
-    # bot_dp.sql_create()
+async def on_startup(_):
+    asyncio.create_task(notification.scheduler())
+    bot_dp.sql_create()
 
 
-# fsmAdminMenu.register_handlers_fsmAdminMenu(dp)
-# notification.register_handlers_notification(dp)
-# client.register_client_handlers(dp)
-# callback.register_callback_handlers(dp)
-# admin.register_admin_handlers(dp)
-
+fsmAdminMenu.register_handlers_fsmAdminMenu(dp)
+notification.register_handlers_notification(dp)
+client.register_client_handlers(dp)
+callback.register_callback_handlers(dp)
 extra.register_extra_handlers(dp)
 
 if __name__=="__main__":
-    executor.start_polling(dp, skip_updates=True,)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
 
 
 
